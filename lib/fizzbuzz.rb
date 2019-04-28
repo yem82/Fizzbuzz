993525c0c21eb421a870fc4dcb26c0df58af3509
Yemi <yemiadebayo60@gmail.com>
2019-04-28 23:04:12 +0100

    Add production code

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,6 +1,13 @@
-class Fizzbuzz
-  def fizz(n)
-    puts "fizz" if n == 3
-  end	
-end
+
+def fizzbuzz(n)
+  if n%15 == 0
+  	"fizzbuzz"
+  elsif n%3 == 0
+  	"fizz" 
+  elsif n%5 == 0
+  	"buzz" 
+  else
+  	n
+  end
+end	
 
